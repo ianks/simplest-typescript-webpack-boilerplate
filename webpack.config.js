99dc2c1a229b9b25367f3faa9dc85be5f9b0f155
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-12-17 00:09:48 -0500

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./index.ts",
+  output: {
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
+  module: {
+    loaders: [
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  }
+}
